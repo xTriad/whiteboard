6051879ecc4ba040fc20bf34a156ae8381746c3d
Kip Nichols <kip_nichols@baylor.edu>
2013-02-08 17:07:41 -0600

    Integrated database with the base app.

--- db/migrate/20130205185201_rolify_create_roles.rb
+++ db/migrate/20130205185201_rolify_create_roles.rb
@@ -1,19 +1,19 @@
 class RolifyCreateRoles < ActiveRecord::Migration
   def change
     create_table(:roles) do |t|
-      t.string :name
+      t.string :role_name
       t.references :resource, :polymorphic => true
 
       t.timestamps
     end
 
-    create_table(:users_roles, :id => false) do |t|
+    create_table(:user_roles, :id => false) do |t|
       t.references :user
       t.references :role
     end
 
-    add_index(:roles, :name)
-    add_index(:roles, [ :name, :resource_type, :resource_id ])
-    add_index(:users_roles, [ :user_id, :role_id ])
+    add_index(:roles, :role_name)
+    add_index(:roles, [ :role_name, :resource_type, :resource_id ])
+    add_index(:user_roles, [ :user_id, :role_id ])
   end
 end
