2814b14cce70fe4e6bc7c7cc55073691e87806dc
Jordan <xtriad@gmail.com>
2013-03-18 17:44:55 -0500

    RSpec tests now run properly
    Rspec was failing due to broken queries to the database because the valid_attributes hash didn't contain all of the necessary default values needed to query the database. For example, creating a Section was failing in the tests because no value for the number attribute was specified, so it was null, and the database won't allow a null value when creating a Section.

--- spec/controllers/assignment_types_controller_spec.rb
+++ spec/controllers/assignment_types_controller_spec.rb
@@ -24,7 +24,9 @@ describe AssignmentTypesController do
   # AssignmentType. As you add validations to AssignmentType, be sure to
   # update the return value of this method accordingly.
   def valid_attributes
-    { "name" => "MyText" }
+    {
+      :name => "MyText"
+    }
   end
 
   # This should return the minimal set of values that should be in the session
