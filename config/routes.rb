e17f17e8ffe815d5bc7a909d7a433a054af1f808
xTriad <xTriad@gmail.com>
2013-03-23 15:07:06 -0500

    Fixed routing
    Each course now only shows the sections it contains, and each section now only shows the assignment it contains.

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,12 @@ Whiteboard::Application.routes.draw do
   # TODO: Need to get rid of whiteboard.com/uploads
   # Upload page should only exist at whiteboard.com/courses/id/sections/id/assignments/id/edit
   # See http://stackoverflow.com/questions/3589884/what-does-map-resource-in-the-route-file-do-exactly/3590622#3590622
-  # and http://guides.rubyonrails.org/routing.html @ 2.7.1 Limits to Nesting
+  # and http://guides.rubyonrails.org/routing.html
+  #       2.7.1 Limits to Nesting
+  #       2.5 Singular Resources (make profile go to currently logged in user's profile)
+  #            match "profile" => "users#show"
+  #       3.2 Dynamic Segments
+  #            match ':controller/:action/:id/:user_id'
 
   # whiteboard.com/courses/id/sections/id/assignments/id
   resources :courses do
@@ -26,7 +31,7 @@ Whiteboard::Application.routes.draw do
     root :to => 'home#index'
   end
 
-  root :to => "home#index"
+  root :to => 'home#index'
 
   devise_for :users
   resources :users
