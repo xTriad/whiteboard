4dc5a83ba4707f0a8c7dc64962f7bd243ec039e1
xTriad <xTriad@gmail.com>
2013-04-03 21:54:16 -0500

    Added attendances to seeds.rb

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -1,5 +1,5 @@
 class Attendance < ActiveRecord::Base
-   attr_accessible :section_id, :user_id, :class_date, :present, :absent, :tardy, :excused
+   attr_accessible :attendance_id, :section_id, :user_id, :class_date, :present, :absent, :tardy, :excused
 
   # May be easier to just use the Attendance.all.to_json wherever you need it.
   def allToJson
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,13 +8,21 @@
 # Environment variables (ENV['...']) are set in the file config/application.yml.
 # See http://railsapps.github.com/rails-environment-variables.html
 
+# Very useful post: http://stackoverflow.com/a/10302357
+# Tip: Use rake db:reset to drop the database, re-create it, and run this seeds file
+#   Note that this may not retain some foreign key restraints for some reason
+
 # Insert Universities
 puts 'DEFAULT UNIVERSITIES'
 University.create([
   { :university_name => 'Baylor' },
   { :university_name => 'Texas' },
-  { :university_name => 'A&M' }
+  { :university_name => 'Tech' }
 ])
+
+Baylor = 1
+Texas = 2
+Tech = 3
 
 # Insert Roles
 puts 'DEFAULT ROLES'
@@ -23,26 +31,43 @@ YAML.load(ENV['ROLES']).each do |role|
   puts 'role: ' << role
 end
 
-# Insert Default Users
+# Insert Users
 puts 'DEFAULT USERS'
-user = User.find_or_create_by_email(
-  :name => ENV['ADMIN_NAME'].dup,
-  :email => ENV['ADMIN_EMAIL'].dup,
-  :password => ENV['ADMIN_PASSWORD'].dup,
-  :password_confirmation => ENV['ADMIN_PASSWORD'].dup,
-  :university_id => 1
-)
 
-puts 'user: ' << user.name
-user.add_role :admin
+  # Main admin
+  user = User.find_or_create_by_email(
+    :name => ENV['ADMIN_NAME'].dup,
+    :email => ENV['ADMIN_EMAIL'].dup,
+    :password => ENV['ADMIN_PASSWORD'].dup,
+    :password_confirmation => ENV['ADMIN_PASSWORD'].dup,
+    :university_id => Baylor
+  )
+  user.add_role :admin
+  Admin_UID = 1
+
+User.create([
+  {
+    :name => 'user1',
+    :email => 'user1@whiteboard.com',
+    :password => 'password',
+    :password_confirmation => 'password',
+    :university_id => Baylor
+  },
+  {
+    :name => 'user2',
+    :email => 'user2@whiteboard.com',
+    :password => 'password',
+    :password_confirmation => 'password',
+    :university_id => Baylor
+  }
+])
 
 # Copy Users to Active Admin
 adminuser = AdminUser.create!(
   :email => ENV['ADMIN_EMAIL'].dup,
   :password => ENV['ADMIN_PASSWORD'].dup,
   :password_confirmation => ENV['ADMIN_PASSWORD'].dup,
-  # :admin => true,
-  :university_id => 1
+  :university_id => Baylor
 )
 
 # Insert Courses
@@ -50,42 +75,42 @@ puts 'DEFAULT COURSES'
 Course.create([
   {
     :number => 1430,
-    :university_id => 1,
+    :university_id => Baylor,
     :name => 'Introduction I'
   },
   {
     :number => 1440,
-    :university_id => 1,
+    :university_id => Baylor,
     :name => 'Introduction II'
   },
   {
     :number => 2334,
-    :university_id => 1,
+    :university_id => Baylor,
     :name => 'Introduction to Systems'
   },
   {
     :number => 3101,
-    :university_id => 1,
+    :university_id => Baylor,
     :name => 'Computer Ethics'
   },
   {
     :number => 3344,
-    :university_id => 1,
+    :university_id => Baylor,
     :name => 'Data Structures'
   },
   {
     :number => 3335,
-    :university_id => 1,
+    :university_id => Baylor,
     :name => 'Database'
   },
   {
     :number => 4321,
-    :university_id => 1,
+    :university_id => Baylor,
     :name => 'Computer Networking'
   },
   {
     :number => 4330,
-    :university_id => 1,
+    :university_id => Baylor,
     :name => 'Foundations'
   }
 ])
@@ -107,21 +132,55 @@ AssignmentType.create([
   { :name => 'Test' }
 ])
 
+Homework = 1
+Program = 2
+Quiz = 3
+Test = 4
+
 # Insert Assignments
 puts 'DEFAULT ASSIGNMENTS'
 Assignment.create([
   {
-    :type_id => 1,
+    :type_id => Homework,
     :section_id => 1,
     :due_date => '2020-02-12',
     :highest_grade_value => 100,
     :weight => 1
   },
   {
-    :type_id => 1,
+    :type_id => Homework,
     :section_id => 2,
     :due_date => '2020-02-14',
     :highest_grade_value => 90,
     :weight => 2
+  }
+])
+
+# Insert Attendances
+puts 'DEFAULT ATTENDANCES'
+Attendance.create([
+  {
+    :attendance_id => 1,
+    :section_id => 1,
+    :user_id => Admin_UID,
+    :class_date => DateTime.new(2013,4,1)
+  },
+  {
+    :attendance_id => 2,
+    :section_id => 1,
+    :user_id => 2,
+    :class_date => DateTime.new(2013,4,1)
+  },
+  {
+    :attendance_id => 3,
+    :section_id => 1,
+    :user_id => Admin_UID,
+    :class_date => DateTime.now
+  },
+  {
+    :attendance_id => 4,
+    :section_id => 1,
+    :user_id => 2,
+    :class_date => DateTime.now
   }
 ])
