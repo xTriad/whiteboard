075e8d18cc46fae0c1616424e32b5dc52c14676d
xTriad <xTriad@gmail.com>
2013-04-07 21:38:01 -0500

    Oops
    Forgot to add this file on the last commit. It's used primarily in the seeds.rb file, but also in queries throughout the app so we don't have to remember, say, the role id for a Professor.

---
+++ config/initializers/constants.rb
@@ -0,0 +1,25 @@
+# Role IDs
+Admin_RID     = 1
+Professor_RID = 2
+TA_RID        = 3
+Student_RID   = 4
+Observer_RID  = 5
+
+# University IDs
+Baylor = 1
+Texas = 2
+Tech = 3
+
+# AssignmentType IDs
+Homework = 1
+Program = 2
+Quiz = 3
+Test = 4
+
+# Test account IDs
+Admin_UID      = 1
+Professor1_UID = 2
+TA1_UID        = 3
+Student1_UID   = 4
+Student2_UID   = 5
+Student3_UID   = 6
