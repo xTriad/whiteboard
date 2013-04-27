3644df7bb596f3bcb968df6ad81f320c56a7f1ed
Kathleen <silverfireflute@yahoo.com>
2013-04-27 13:50:57 -0500

    Guard starts automatically with rails s
    config/initializers/start_guard.rb

---
+++ config/initializers/start_guard.rb
@@ -0,0 +1,7 @@
+require 'guard'
+
+# This line works, but watches at whiteboard root dir:
+ # Guard.start(:guardfile => 'storage/Guardfile')
+
+ # Watches at /storage
+ Guard.start(:watchdir => 'storage')
