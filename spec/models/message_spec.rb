7cd85d4e2643fb416f05840fdf869b325e7f5d83
Kathleen <silverfireflute@yahoo.com>
2013-04-29 20:21:14 -0500

    Rspec tests added but don't compile yet.

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -1,5 +1,44 @@
 require 'spec_helper'
 
 describe Message do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  before(:each) do
+    @attr = {
+      :message_id => "10",
+      :sender_id=> "2",
+      :receiver_id => "4",
+      :date_sent => "2013-21-2"
+    }
+
+    @attr2 = {
+      :message_id => "10",
+      :sender_id=> "2",
+      :receiver_id => "2",
+      :date_sent => "2013-21-2"
+    }
+
+
+  end
+
+  it "should create a new instance given a valid attribute" do
+    Message.create!(@attr)
+  end
+
+  it "should require a sender_id" do
+    no_si = Message.new(@attr.merge(:sender_id => ""))
+    no_si.should_not be_valid
+  end
+
+  it "should require a receiver_id" do
+    no_ri = Message.new(@attr.merge(:receiver_id => ""))
+    no_ri.should_not be_valid
+  end
+
+  it "should reject the same sender and receiver id" do
+    same_number=Message.create!(@attr2)
+    same_number.should_not_be_valid
+
+  end
+
+
 end
--- spec/models/universities_spec.rb
+++ spec/models/universities_spec.rb
@@ -1,5 +1,29 @@
 require 'spec_helper'
 
-describe Universities do
-  pending "add some examples to (or delete) #{__FILE__}"
+ describe Universities do
+
+  before(:each) do
+    @attr = {
+      :university_id => "5",
+      :university_name => "LSU",
+    }
+  end
+
+  it "should create a new instance given a valid attribute" do
+    Univesity.create!(@attr)
+  end
+
+  it "should require a university name" do
+    no_name = University.new(@attr.merge(:university_name => ""))
+    no_name.should_not be_valid
+  end
+
+  it "should require a number" do
+    no_number = University.new(@attr.merge(:university_id) => ""))
+    no_number.should_not be_valid
+  end
+
 end
+
+
+
