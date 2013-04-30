e5784925585f2ab7c0a5306b65cba2ecd2b22587
xTriad <xtriad@gmail.com>
2013-04-29 19:45:22 -0500

    Active Admin university_id restrictions
    Admins can now only view data in their own university. However, filters are broken, and admins can see all courses and sections on the Sections and Sections Users Roles tabs.

--- app/admin/courses.rb
+++ app/admin/courses.rb
@@ -1,22 +1,34 @@
 ActiveAdmin.register Course do
-#=begin
-  index do                           
+
+  controller do
+   def index
+      index! do |format|
+        the_uni_id = AdminUser.find(current_admin_user.id).university_id
+        @courses = Course.where(:university_id => the_uni_id).page(params[:page])
+        format.html
+      end
+    end
+  end
+
+  index do
     column :number
     column :name
     column :created_at
     column :updated_at
-    default_actions                   
-  end                                 
+    default_actions
+  end
   
   filter :name
   filter :number
 
-  form do |f|                         
-    f.inputs "Course Details" do       
+  form do |f|
+    the_uni_id = AdminUser.find(current_admin_user.id).university_id
+
+    f.inputs "Course Details" do
       f.input :name
       f.input :number
-    end                               
-    f.actions                         
+      f.input :university_id, :as => :hidden, :value => the_uni_id
+    end
+    f.actions
   end
-#=end
 end
