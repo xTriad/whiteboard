be20cba4225563025eee12ecfc1616cfdb91246c
xTriad <xTriad@gmail.com>
2013-05-01 10:49:10 -0500

    Guardfile - failure

--- lib/update_assignment_upload_list.rb
+++ lib/update_assignment_upload_list.rb
@@ -1,2 +1,13 @@
 puts "Executing update_assignment_upload_list.rb"
 
+file_name = "filename"
+content_type = "content-type"
+file_size = 120
+created_at = DateTime.now
+updated_at = DateTime.now
+user_id = 6
+assignment_id = 11
+
+query = "INSERT INTO assignment_uploads (file_name, content_type, file_size, created_at, updated_at, user_id, assignment_id) VALUES (#{file_name}, #{content_type}, #{file_size}, #{created_at}, #{updated_at}, #{user_id}, #{assignment_id})"
+
+ActiveRecord::Base.connection.execute(query);
--- storage/helloworld_cpp/user2/helloworld.cpp
+++
@@ -1,9 +0,0 @@
-
-#include <iostream>
-
-using namespace std;
-
-int main() {
-	cout << "Hello Capstone in C++!" << endl;
-	return 0;
-}
